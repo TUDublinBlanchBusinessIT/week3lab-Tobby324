17acdc7bfdfd7d47e97c97d55676019d12e15638
B00165442 Tobby Akinwale <B00165442@mytudublin.ie>
2025-02-17 17:57:50 +0000

    Completed Step 3

--- findFirstFourPNs.py
+++ findFirstFourPNs.py
@@ -1,6 +1,19 @@
-#Write a program here that finds the first four perfect numbers.
-#Put your name and date at the top of the file.
-#The pseudocode in the README.md file will help you write this program.
+#Tobby Akinwale
+#17/02/2025
+#findFirstFourPNs
+
+from perfectNumber import isPerfectNumber
+
+def find_first_four_perfect_number():
+    count = 0
+    num = 1
+    while count < 4:
+        if isPerfectNumber (num):
+            print(f"{num} is a perfct number")
+            count +=1
+        num += 1
+        
+find_first_four_perfect_numbers()
 
 
 
